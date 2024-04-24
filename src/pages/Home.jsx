109457a7fd846ae1f18385a0d052e322021f2509
OdiljonMirzaxamdamov <odiojon1994@gmail.com>
2024-04-24 23:13:16 +0500

    17/3 - Переносим бизнес-логику вывода пиццы из Home в редакс и Делаем получение пицц с бэкенда используя createAsyncThunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import axios from "axios";
 import qs from 'qs';
 import { useNavigate } from "react-router-dom";
 
 import { SearchContext } from "../App"; // Это у нас подключение поиска с помощью юзКонтекста
-import {useDispatch, useSelector} from 'react-redux'; // Это у нас подключение категорий и сортировки с помощью Редакса
+import { useDispatch, useSelector } from 'react-redux'; // Это у нас подключение категорий и сортировки с помощью Редакса
 import { setFilters } from "../redux/slices/filterSlice";
+import { fetchPizzas } from "../redux/slices/pizzaSlice";
 
 import Categories from "../components/Categories";
 import Sort, {sortList} from "../components/Sort";
@@ -21,52 +21,58 @@ export const Home = () => {
     const isMounted = React.useRef();
 
     const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+    const { items, status } = useSelector((state) => state.pizza);
+
     const { searchValue } = React.useContext(SearchContext);
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
 
 
-    const fetchPizzas = async () => {
-        setIsLoading(true)
-
+    const getPizzas = async () => {
         const category = categoryId > 0 ? `&category=${categoryId}` : '';
         const sortBy = `&sortBy=${sort.sortProperty}`;
         const search = searchValue ? `&title=*${searchValue}` : '';
         const pagination = `page=${currentPage}&limit=4`;
 
-        try {
-            const resMetaPagination = await axios.get(
-                `https://17d2006fd5b63307.mokky.dev/items?${pagination}${category}${search}${sortBy}`
-            ); console.log(resMetaPagination)
-            setItems(resMetaPagination.data.items);
-        }
-        catch (error) {
-            console.log('ERROR', error);
-            alert('Ошибка при получении пицц из бэкэнда');
-        }
-        finally {
-            setIsLoading(false);
-        }
+        dispatch(
+            fetchPizzas({
+                category,
+                sortBy,
+                search,
+                pagination
+            }),
+        );
+
+        window.scrollTo(0, 0);
     };
 
 
     // Если изменили параметры и был первый рендер
     React.useEffect(() => {
         if (isMounted.current) {
-            const queryString = qs.stringify({
+            const params = {
+                categoryId: categoryId > 0 ? categoryId : null,
                 sortProperty: sort.sortProperty,
-                categoryId,
-                currentPage,
-            });
+                currentPage
+            };
 
-            navigate(`?${queryString}`)
+            const queryString = qs.stringify(params, {skipNulls: true});
+
+            navigate(`/?${queryString}`)
         }
 
-        isMounted.current = true;
-    }, [categoryId, sort.sortProperty, currentPage]);
+        if (window.location.search) {
+            fetchPizzas();
+        };
+
+    }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
 
-    // Если был первый рендер, то проверяем URL-параметры и сохраняем в редаксе
+    // Если был первый рендер, то запрашиваем пиццы
+    React.useEffect(() => {
+        getPizzas();
+    }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+
+    //!!!!!!! Если был первый рендер, то проверяем URL-параметры и сохраняем в редаксе
     React.useEffect(() => {
         if (window.location.search) {
             const params = qs.parse(window.location.search.substring(1));
@@ -84,18 +90,6 @@ export const Home = () => {
     }, []);
 
 
-    // Если был первый рендер, то запрашиваем пиццы
-    React.useEffect(() => {
-        window.scrollTo(0, 0); //делает скролл вверх при открытии страницы основной страницы
-
-        if (!isSearch.current) {
-            fetchPizzas();
-        }
-
-        isSearch.current = false;
-    }, [categoryId, sort.sortProperty, searchValue, currentPage]);
-
-
     const skeleton = [...new Array(6)].map((_, index) => <Skeleton key={index}/>);
     const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
@@ -106,7 +100,7 @@ export const Home = () => {
                 <Sort />
             </div>
             <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">{isLoading ? skeleton : pizzas}</div>
+            <div className="content__items">{status === 'loading' ? skeleton : pizzas}</div>
             <Pagination />
         </div>
     )
---
+++ src/redux/slices/pizzaSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios";
+
+
+export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params) => {
+    const { pagination, category, search, sortBy } = params;
+    const { data } = await axios.get(`https://17d2006fd5b63307.mokky.dev/items?${pagination}${category}${search}${sortBy}`);
+    return data.items;
+},);
+
+
+
+const initialState = {
+    items: [],
+    status: 'loading',
+}
+
+
+const pizzaSlice = createSlice({
+    name: 'pizza',
+    initialState,
+
+    reducers: {
+        setItems: (state, action) => {
+            state.items = action.payload
+        },
+
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPizzas.pending, (state) => {
+                state.status = "loading"
+                state.items = []
+                console.log(state, 'loading')
+            })
+            .addCase(fetchPizzas.fulfilled, (state, action) => {
+                state.items = action.payload
+                state.status = "success"
+                console.log(state, 'success')
+            })
+            .addCase(fetchPizzas.rejected, (state) => {
+                state.status = "error"
+                state.items = []
+                console.log(state, 'error')
+            })
+    }
+})
+
+
+
+export const { setItems } = pizzaSlice.actions
+
+export default pizzaSlice.reducer
