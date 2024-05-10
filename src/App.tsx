74118cbb21192524e870946348b340eac9eb65e5
OdiljonMirzaxamdamov <odiojon1994@gmail.com>
2024-05-10 20:48:34 +0500

    27/1 - Code-splitting, React.lazy, React.loadable-components
    во избежание лишнего рендеринга pages, пользуемся React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import './App.css';
 import './scss/app.scss'
 
-import Home from "./pages/Home";
-import HomeEmpty from "./pages/Home-empty";
-import Cart from "./pages/Cart";
-import CartEmpty from "./pages/Cart-empty";
-import NotFound from "./components/NotFoundBlock";
-import FullPizza from "./pages/FullPizza";
+// в 19-уроке подключили Outlet компонент MainLayout, оно используется для больших проектов, тут мы делаем для примера
 import MainLayout from "./layouts/MainLayout";
+
+// в 27-уроке узнал про React.lazy/React.loadable-components для Code-splitting(загрузка компонентов по очереди)
+// по сути это своего рода Promise (.then), выполняет загрузку только когда это необходимо.
+// тут мы все страницы, кроме Home, обернём в метод Lazy, он заставляет загрузиться страницу только тогда-когда будет открыта данное окно
+// окно Home оставляем, чтобы он открывался сразу же при первом рендере.
+// Suspense - это такой метод который говорит fallback, в моменте когда идет загрузка.
+import Home from "./pages/Home";
+const HomeEmpty = lazy(() => import("./pages/Home-empty"));
+const Cart = lazy(() => import("./pages/Cart"));
+const CartEmpty = lazy(() => import("./pages/Cart-empty"));
+const FullPizza = lazy(() => import("./pages/FullPizza"));
+const NotFound = lazy(() => import("./components/NotFoundBlock"));
 
 
 function App() {
@@ -17,15 +25,35 @@ function App() {
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route path="" element={<Home/>}/>
-                <Route path="home-empty" element={<HomeEmpty/>}/>
-                <Route path="cart" element={<Cart/>}/>
-                <Route path="cart-empty" element={<CartEmpty/>}/>
-                <Route path="pizza/:id" element={<FullPizza/>}/>
-                <Route path="*" element={<NotFound/>}/>
+                    <Route path="home-empty" element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <HomeEmpty />
+                        </Suspense>}
+                    />
+                    <Route path="cart" element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Cart />
+                        </Suspense>}
+                    />
+                    <Route path="cart-empty" element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <CartEmpty />
+                        </Suspense>}
+                    />
+                    <Route path="pizza/:id" element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <FullPizza />
+                        </Suspense>}
+                    />
+                    <Route path="*" element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <NotFound />
+                        </Suspense>}
+                    />
             </Route>
         </Routes>
     );
 }
-// в 19-уроке подключили Outlet компонент MainLayout, оно используется для больших проектов, тут мы делаем для примера
+
 
 export default App;
